60594425d1ee8dc0231a830a0964f0c184e8f573
Dawid Metelski <metel628@onet.pl>
2022-05-03 18:18:48 +0200

    Complete User System with Identity

---
+++ CheckersApp/Server/Data/Migrations/20220503152633_aplicationUser.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CheckersApp.Server.Data.Migrations
+{
+    public partial class aplicationUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Score",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Score",
+                table: "AspNetUsers");
+        }
+    }
+}
